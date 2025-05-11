3808b9fc4c8334d28a59956697bc27a54ed4e59e
Amul Gaurav <amulgaurav907@gmail.com>
2025-05-11 12:57:11 +0530

    bug fixes

--- common/src/types/blog.ts
+++ common/src/types/blog.ts
@@ -1,14 +1,14 @@
 import { z } from "zod";
 
 export const createBlogInput = z.object({
-  title: z.string().email().min(5).max(40),
+  title: z.string().min(5).max(40),
   content: z.string().min(6).max(60),
   published: z.boolean().optional(),
 });
 
 export const updateBlogInput = z.object({
   id: z.string().max(50),
-  title: z.string().email().min(5).max(40).optional(),
+  title: z.string().min(5).max(40).optional(),
   content: z.string().min(6).max(60).optional(),
   published: z.boolean().optional().optional(),
 });
