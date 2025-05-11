2bb6969045cbf8ec72f471d190e77968c3b2585c
Amul Gaurav <amulgaurav907@gmail.com>
2025-05-11 20:08:02 +0530

    feat: Add Dark mode using shadcn

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,26 +1,31 @@
 import "./App.css";
 import { Routes, Route } from "react-router";
+import { ThemeProvider } from "./context/themeProvider";
 import Blog from "./pages/Blog";
 import Signup from "./pages/Signup";
 import Signin from "./pages/Signin";
+import Navbar from "./components/Navbar";
 
 function App() {
   return (
-    <Routes>
-      <Route index element={<Blog />} />
-      {/* <Route path="about" element={<About />} /> */}
-
-      {/* <Route element={<AuthLayout />}> */}
-      <Route path="login" element={<Signin />} />
-      <Route path="signup" element={<Signup />} />
-      {/* </Route> */}
-
-      <Route path="blogs">
+    <ThemeProvider defaultTheme="dark" storageKey="vite-ui-theme">
+      <Navbar />
+      <Routes>
         <Route index element={<Blog />} />
-        {/* <Route path=":city" element={<City />} />
+        {/* <Route path="about" element={<About />} /> */}
+
+        {/* <Route element={<AuthLayout />}> */}
+        <Route path="login" element={<Signin />} />
+        <Route path="signup" element={<Signup />} />
+        {/* </Route> */}
+
+        <Route path="blogs">
+          <Route index element={<Blog />} />
+          {/* <Route path=":city" element={<City />} />
         <Route path="trending" element={<Trending />} /> */}
-      </Route>
-    </Routes>
+        </Route>
+      </Routes>
+    </ThemeProvider>
   );
 }
 
