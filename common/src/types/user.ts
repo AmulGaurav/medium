19b3b57371645e5fc37265eb16a2fb2a32447c6c
Amul Gaurav <amulgaurav003.com>
2025-05-13 18:21:40 +0530

    common: make name field optional in signupInput

--- common/src/types/user.ts
+++ common/src/types/user.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 export const signupInput = z.object({
   email: z.string().email().min(5).max(40),
   password: z.string().min(6).max(20),
-  name: z.string().min(1).max(40).optional(),
+  name: z.string().min(1).max(40),
 });
 
 export const signinInput = z.object({
