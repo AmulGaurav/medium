d28a3d9368d5ade5a2bc5bafc294fb76d8237300
Amul Gaurav <amulgaurav003.com>
2025-05-13 12:55:35 +0530

    server: Enable cors

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,8 +1,16 @@
 import { Hono } from "hono";
+import { cors } from "hono/cors";
 import { blogRouter } from "./routes/blog";
 import { userRouter } from "./routes/user";
 
 const app = new Hono();
+
+app.use(
+  "/api/v1/*",
+  cors({
+    origin: "http://localhost:5173",
+  })
+);
 
 app.get("/", (c) => {
   return c.text("Hello Hono!");
