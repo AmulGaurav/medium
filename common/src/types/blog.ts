421a8a5b2ba4c7ce1d8ebacd7b64047514972b7d
Amul Gaurav <amulgaurav003@gmail.com>
2025-05-16 18:51:01 +0530

    common: Increased content length in blog & add error messages

--- common/src/types/blog.ts
+++ common/src/types/blog.ts
@@ -1,8 +1,13 @@
 import { z } from "zod";
 
 export const createBlogInput = z.object({
-  title: z.string().min(5).max(40),
-  content: z.string().min(6).max(60),
+  title: z
+    .string()
+    .min(5, { message: "Title must include at least 5 characters!" })
+    .max(60, { message: "Title cannot include more than 60 characters!" }),
+  content: z.string().min(6, { message: "Content is too short!" }).max(2000, {
+    message: "Maximum limit of 2000 characters has been reached!",
+  }),
   published: z.boolean().optional(),
 });
 
